091e68b4937340e85bc1abf5d5261afe35b5dee5
Marcin Kaminski <marcin.kaminski@schibsted.com>
2020-05-10 18:12:22 +0200

    ⚡️ Added more obstacles and improved shuffle func

--- src/utils/math-util.js
+++ src/utils/math-util.js
@@ -1,5 +1,5 @@
 export const random = (min, max) => Math.random() * (max - min) + min;
 
-export const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+export const shuffle = (arr) => arr.sort(() => Math.random() > 0.5);
 
 export const round = (number, precision) => Number(number.toFixed(precision));
